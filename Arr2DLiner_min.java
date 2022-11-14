7d793e4f6707f3c0d640fa21c156c392c9a70af6
chetansmali <cmali4596@gmail.com>
2022-11-14 20:34:37 +0530

    It's an 2D array, Find the minimun element by using Integer.MAX_VALUE (IF find min use MAX_VALUE or if max use MIN_VALUE)

--- Arr2DLiner_min.java
+++ Arr2DLiner_min.java
@@ -2,6 +2,22 @@ import javax.sound.sampled.SourceDataLine;
 
 public class Arr2DLiner_min {
     public static void main(String[] args) {
-        System.out.println("hello varsha..!");
+        int[][] arr={
+            {24,2,12,13,65,055},
+            {84,956,-77},
+            {78,1,45,3}
+        };
+        System.out.println(LinMin(arr));
+    }
+    static int LinMin(int[][] arr){
+        int min=Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                    if(arr[i][j]<min) {
+                        min=arr[i][j];
+                    }              
+            }
+        }
+        return min;
     }
 }
