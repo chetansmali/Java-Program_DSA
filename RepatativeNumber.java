50353ee2af917ac43f4a9e735e0477a5a0e85634
chetansmali <cmali4596@gmail.com>
2022-12-08 03:39:13 +0530

    Finding the number start and end of repiting numbers position

--- RepatativeNumber.java
+++ RepatativeNumber.java
@@ -3,6 +3,7 @@ public class RepatativeNumber {
     public static void main(String[] args) {
         int[] arr={1,2,4,5,5,6,6,6,6,7,8};
         int t=6;
+
         System.out.println(Arrays.toString(rep(arr,t)));
     }
     static int[] rep(int[] arr,int t){
