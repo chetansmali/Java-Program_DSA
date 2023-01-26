277add964e821ee3a6c17c5acc4079f78e9d0a69
chetansmali <cmali4596@gmail.com>
2023-01-26 10:58:09 +0530

    Fibonaccies series

---
+++ Fibonaccis.java
@@ -0,0 +1,13 @@
+class Fibonaccis {
+    public static void main(String[] args) {
+        System.out.println(fibo(5));
+    }
+
+    //Fibonaccies using recursion
+    static int fibo(int n){
+        if (n < 2){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+}
