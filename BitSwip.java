82deae78d321df72660e72ddb298c4cc5eeadb49
chetansmali <cmali4596@gmail.com>
2023-01-29 17:00:10 +0530

    Swipping two nummbers using bit operation

--- BitSwip.java
+++ BitSwip.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class BitSwip {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first number :");
         int n=sc.nextInt();
+	System.out.println("Enter a secound number :");
         int n1=sc.nextInt();
         System.out.println(Arrays.toString(bitSwip(n,n1)));
     }
