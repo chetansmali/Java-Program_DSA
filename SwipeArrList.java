193399e6ff838e97af4aa9e10681ff924be03485
varshakaveri <varsha.kaveri24@gmail.com>
2023-08-14 11:29:22 +0530

    Swiping array fuction

--- SwipeArrList.java
+++ SwipeArrList.java
@@ -11,6 +11,7 @@ public class SwipeArrList {
         System.out.println(max);
     }
 
+//swiping the array elements.
     static int swip(int[] arr){
         int max=arr[0];
         for (int i = 0; i < arr.length; i++) {
