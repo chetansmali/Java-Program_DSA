81867953d4b770d0b258bdd5fc70dd750f1b5546
chetansmali <cmali4596@gmail.com>
2023-09-08 17:55:11 +0530

    Pattern n*2(

--- Pattern/Pattern2.java
+++ Pattern/Pattern2.java
@@ -5,7 +5,7 @@ public class Pattern2 {
         int n = 5;
          for(int r=1;r<=n*2;r++)
          {
-            int d= r > n ? n*2 - r : r;
+            int d= r > n ? n*2 - r : n-r;
             for(int c=1;c<d;c++){
                 System.out.print(" *");
             }
